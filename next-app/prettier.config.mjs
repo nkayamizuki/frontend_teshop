6ba15171791b03b8a4bbc0c67ae50e885eaacf24
nkayamizuki <5991kazuki@gmail.com>
2025-02-24 07:40:32 +0000

    eslintとprettierのインストール

---
+++ next-app/prettier.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  endOfLine: 'lf',
+};
+
+export default config;
